280dad059912f78daf8f1e84f6aa81087387df88
shark123YT <137716913+shark123YT@users.noreply.github.com>
2023-06-26 21:40:48 +0200

    Delete TESTmod2.js

--- TESTmod2.js
+++
@@ -1,9 +0,0 @@
-// Adding elements:
-elements.Cryotheum = {
-    color: "#133738",
-    behavior: behaviors.LIQUID,
-    category: "liquids",
-    viscosity: 10,
-    state: "liquid",
-    density: 1200,
-};
